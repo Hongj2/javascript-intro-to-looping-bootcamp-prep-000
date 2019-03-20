ab5d3562e5e281aeafa3d344affc1ffa7357c565
Jessica Hong <jessicahong.24@gmail.com>
2019-03-20 01:45:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -27,4 +27,17 @@ function doWhileLoop(num){
     console.log("I run once regardless.")
   } while (incrementVariable()<num);
   }
+
+function doWhileLoop(num) {
+  var i = 0;
+    
+ function incrementVariable() {
+        i = i + 1;
+        return i;
+  }
+  
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+
 }
